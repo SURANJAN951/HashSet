fe0914a80a2b3354a06fa1d8040c1da818f27a6d
Suranjan kumar <ksuranjan951@gmail.com>
2024-08-21 12:07:35 +0530

    HashSet

---
+++ HashSetExample.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+
+public class HashSetExample {
+
+    public static void main(String[] args) {
+        HashSet hs = new HashSet();
+        hs.add(100);
+        hs.add(20);
+        hs.add(30);
+        hs.add(40);
+        System.out.println(hs);
+    }
+}
